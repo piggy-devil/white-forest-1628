7e44869e1a781a766870bff217d251fb730c833b
piggy-devil <muupaza@gmail.com>
2024-12-09 17:11:50 +0700

    cloudflareLoader

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,18 +7,18 @@ import { useSession } from "next-auth/react";
 const HomePage = () => {
   const { data: session } = useSession();
 
+  console.log("session : ", session);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center">
-        {session?.user?.image && (
-          <MyImage
-            src={session.user.image}
-            alt="User Image"
-            width={48}
-            height={48}
-            className="rounded-full"
-          />
-        )}
+        <MyImage
+          src={session?.user?.image!}
+          alt="User Image"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
         <SignOut />
       </div>
     </main>
