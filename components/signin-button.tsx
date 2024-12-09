fcb817ffd88a0117addf06ed7ebb0b2d15f8107e
piggy-devil <muupaza@gmail.com>
2024-12-09 14:27:06 +0700

    github -> google

--- components/signin-button.tsx
+++ components/signin-button.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import { signIn } from "next-auth/react";
 
 export function SignIn() {
   return (
-    <button onClick={() => signIn("github", { redirectTo: "/home" })}>
-      Sign In
+    <button onClick={() => signIn("google", { redirectTo: "/home" })}>
+      Login
     </button>
   );
 }
