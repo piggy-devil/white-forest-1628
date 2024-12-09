bf1ba9361ac75e1ee42a2422e43aa83e19cb4dae
piggy-devil <muupaza@gmail.com>
2024-12-09 19:30:37 +0700

    AUTH_SECRET

--- auth.ts
+++ auth.ts
@@ -33,6 +33,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   adapter: ConvexAdapter,
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async session({ session }) {
       const privateKey = await importPKCS8(
